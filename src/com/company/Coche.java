bc8b538109df039b3a4045b3519a21ab86f806ed
Pau111111 <paugarciapellicer96@gmail.com>
2016-09-16 17:32:02 +0200

    Ahora esta supah dupah

--- src/com/company/Coche.java
+++ src/com/company/Coche.java
@@ -1,15 +1,20 @@
 package com.company;
 
-/**
- * Created by usu26 on 16/09/2016.
- */
 public class Coche {
-
     private String marca;
     private String modelo;
     private String matricula;
-    private int potencia;
-    private int precio;
+    private Integer potencia;
+    private Double precio;
+
+
+    public Coche(String marca, String modelo, String matricula, Integer potencia, Double precio) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.matricula = matricula;
+        this.potencia = potencia;
+        this.precio = precio;
+    }
 
     public String getMarca() {
         return marca;
@@ -35,19 +40,55 @@ public class Coche {
         this.matricula = matricula;
     }
 
-    public int getPotencia() {
+    public Integer getPotencia() {
         return potencia;
     }
 
-    public void setPotencia(int potencia) {
+    public void setPotencia(Integer potencia) {
         this.potencia = potencia;
     }
 
-    public int getPrecio() {
+    public Double getPrecio() {
         return precio;
     }
 
-    public void setPrecio(int precio) {
+    public void setPrecio(Double precio) {
         this.precio = precio;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Coche coche = (Coche) o;
+
+        if (marca != null ? !marca.equals(coche.marca) : coche.marca != null) return false;
+        if (modelo != null ? !modelo.equals(coche.modelo) : coche.modelo != null) return false;
+        if (matricula != null ? !matricula.equals(coche.matricula) : coche.matricula != null) return false;
+        if (potencia != null ? !potencia.equals(coche.potencia) : coche.potencia != null) return false;
+        return precio != null ? precio.equals(coche.precio) : coche.precio == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = marca != null ? marca.hashCode() : 0;
+        result = 31 * result + (modelo != null ? modelo.hashCode() : 0);
+        result = 31 * result + (matricula != null ? matricula.hashCode() : 0);
+        result = 31 * result + (potencia != null ? potencia.hashCode() : 0);
+        result = 31 * result + (precio != null ? precio.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Coche{" +
+                "marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", matricula='" + matricula + '\'' +
+                ", potencia=" + potencia +
+                ", precio=" + precio +
+                '}';
     }
 }
